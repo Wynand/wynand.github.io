54c9e796ee4b6069470fd9d827a5178328702633
Wynand Badenhorst <wynandbadenhorst@hotmail.com>
2019-02-04 20:33:52 -0600

    first commit, hope this works

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import * as gitHubLogo from './github-logo.png'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div className="App-logo">
+            <img src={logo} alt="logo" />
+            <img src={gitHubLogo} alt="logo" className = "github-logo" 
+            />
+          </div>
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <a
+            className="App-link"
+            href="https://facebook.github.io/create-react-app/docs/deployment#github-pages-https-pagesgithubcom"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Deploy React with Github Pages
+          </a>
+          <p> Remember to change the 'homepage' value in your <code>package.json</code>! </p>
+          <p> You can clone my source code&nbsp;
+          <a
+            className="App-link"
+            href="https://github.com/wynand/react-gh-pages-template"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            here
+          </a>
+          .
+          </p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
