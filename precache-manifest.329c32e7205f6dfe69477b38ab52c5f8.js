384401e3a51e261f611d82d95ce29104f9543c79
Wynand Badenhorst <wynandbadenhorst@hotmail.com>
2019-02-04 20:53:39 -0600

    Updates

---
+++ precache-manifest.329c32e7205f6dfe69477b38ab52c5f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e6f1b4a4c4de7f5988e4",
+    "url": "/static/js/main.e6f1b4a4.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "e6f1b4a4c4de7f5988e4",
+    "url": "/static/css/main.6cc1fff3.chunk.css"
+  },
+  {
+    "revision": "32258749c056c703c11db27afdbc3d03",
+    "url": "/index.html"
+  }
+];
